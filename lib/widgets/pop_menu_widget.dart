56f9cbf0843755bebd8e0df3310e1d5b32e81570
Adriell8000B <adrielluiz73@gmail.com>
2026-02-11 11:57:19 -0300

    feat: add settings navigation to PopMenuWidget
    - Implemented navigation to the SettingsScreen when the 'Settings' option is selected from the popup menu.
    - Updated the itemBuilder to maintain code organization and readability.

--- lib/widgets/pop_menu_widget.dart
+++ lib/widgets/pop_menu_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/screens/settings_screen.dart';
 
 class PopMenuWidget extends StatelessWidget {
   const PopMenuWidget({super.key});
@@ -6,25 +7,38 @@ class PopMenuWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton<String>(
+      onSelected: (String value) {
+        if (value == 'Settings') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SettingsScreen()),
+          );
+        }
+      },
       iconColor: Colors.grey,
       offset: const Offset(0, 50),
-      itemBuilder: (context) => [
-        'New group',
-        'New community',
-        'Broadcast lists',
-        'Linked devices',
-        'Starred',
-        'Discover business',
-        'Payments',
-        'Read all',
-        'Settings',
-      ].map((choice) => PopupMenuItem(
-        value: choice,
-        child: Text(
-          choice, 
-          style: const TextStyle(color: Colors.white, fontSize: 16),
-        ),
-      )).toList(),
+      itemBuilder: (context) =>
+          [
+                'New group',
+                'New community',
+                'Broadcast lists',
+                'Linked devices',
+                'Starred',
+                'Discover business',
+                'Payments',
+                'Read all',
+                'Settings',
+              ]
+              .map(
+                (choice) => PopupMenuItem(
+                  value: choice,
+                  child: Text(
+                    choice,
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              )
+              .toList(),
     );
   }
 }
