1797ad42bd7613c1e7e342dcfaa1753b78e37db2
Adriell8000B <adrielluiz73@gmail.com>
2026-02-11 11:56:19 -0300

    feat: add SettingsBubbleWidget for settings screen
    - Introduced a new SettingsBubbleWidget to enhance the settings interface.
    - The widget features an icon, title, and subtitle, with an optional onTap callback for interactivity.

---
+++ lib/widgets/settings_bubble_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+
+class SettingsBubbleWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final VoidCallback? onTap;
+
+  const SettingsBubbleWidget({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    this.onTap
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      hoverColor: Colors.white10,
+    );
+  }
+}
